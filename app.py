a2606936fb7f72e8ea4886c62041f0186c7917c2
h.yoshioka <h.yoshioka.sys22@morijyobi.ac.jp>
2024-12-04 10:20:09 +0900

    user_bpをimport

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template , redirect, url_for
 import string,random
+from user import user_bp
 
 app = Flask(__name__)
 app.secret_key = ''.join(random.choices(string.ascii_letters,k=256))
+
+app.register_blueprint(user_bp)
 
 @app.route('/')
 def top():
