9ece85d781f146edc5d0756440a6c833de4daaa1
h.yoshioka <h.yoshioka.sys22@morijyobi.ac.jp>
2025-01-05 15:56:47 +0900

    sessionの確認

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-from flask import Flask, render_template , redirect, url_for,session
-import string,random
+from flask import Flask, render_template, redirect, url_for, session
+import string, random
 from user import user_bp
 
 app = Flask(__name__)
-app.secret_key = ''.join(random.choices(string.ascii_letters,k=256))
+app.secret_key = ''.join(random.choices(string.ascii_letters, k=256))
 
 app.register_blueprint(user_bp)
 
 @app.route('/')
 def top():
-    session.clear()
+    
+    if 'mail' in session:
+        session.pop('mail')
+    
     return render_template('index.html')
 
 if __name__ == '__main__':
