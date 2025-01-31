ab0b04993648cf927a9c208aa45612cd3666d50c
h.yoshioka <h.yoshioka.sys22@morijyobi.ac.jp>
2025-01-31 14:35:20 +0900

    confirmのルーティング

--- health.py
+++ health.py
@@ -17,7 +17,7 @@ class FoodRecord(FlaskForm):
     meal_type = SelectField(
         '食事の種類',
         choices=[("", "選択してください"),("1", "朝食"), ("2", "昼食"), ("3", "夕食")],
-        validators=[DataRequired()]   
+        validators=[DataRequired(message='種類を選択してください')]   
     )
     meal_detail = StringField(
         '食事内容',
@@ -30,3 +30,19 @@ class FoodRecord(FlaskForm):
 def food_record():
     form = FoodRecord()
     return render_template('health/food_record.html',form=form)
+
+@health_bp.route('/food_record_confirm',methods=['GET','POST'])
+def food_record_confirm():
+    form = FoodRecord()
+    
+    date = form.date.data
+    meal_type = form.meal_type.data
+    meal_detail = form.meal_detail.data
+    
+    if form.validate_on_submit():
+        session['date'] = date
+        session['meal_type'] = meal_type
+        session['meal_detail'] = meal_detail
+        return render_template('health/food_record_confirm.html',form=form)
+    else:
+        return render_template('/health/food_record.html',form=form,date=date,meal_type=meal_type,meal_detail=meal_detail)
