509bd9bcbeb96fcbff8a1ad3295092aad62c4599
h.yoshioka <h.yoshioka.sys22@morijyobi.ac.jp>
2025-02-06 18:31:19 +0900

    exercise登録の確認と実行

--- health.py
+++ health.py
@@ -33,11 +33,11 @@ class ExerciseRecord(FlaskForm):
         format='%Y-%m-%d'
     )
     exercise_time = FloatField(
-        '1日の運動時間',
-        validators=[DataRequired(message='1日の運動時間が未入力です'),NumberRange(min=0,max=24)]
+        '運動時間(分)',
+        validators=[DataRequired(message='1日の運動時間が未入力です'),NumberRange(min=0)]
     )
     exercise_detail = StringField(
-        '食事内容',
+        '運動内容',
         validators=[DataRequired()])
     
     submit = SubmitField('記録')
@@ -50,8 +50,6 @@ def food_record():
 @health_bp.route('/food_record_confirm', methods=['POST'])
 def food_record_confirm():
     form = FoodRecord()
-    
-    print("Form Data:", form.meal_detail.data)
     
     if form.validate_on_submit():
         session['date'] = form.date.data
@@ -70,7 +68,6 @@ def food_record_execute():
     meal_type = session.get('meal_type')
     meal_details = session.get('meal_detail')
     user_id = current_user.get_id()
-    print(f"Debug: meal_details - {meal_details}")
 
     meal_id = db.insert_meal(user_id, date, meal_type)
 
@@ -86,7 +83,37 @@ def food_record_execute():
     flash("食事記録が完了しました！", "success")
     return redirect(url_for('user_top'))
 
-@health_bp.route('exercise_record',methods=['GET','POST'])
+@health_bp.route('/exercise_record',methods=['GET','POST'])
 def exercise_record():
     form=ExerciseRecord()
     return render_template('health/exercise_record.html',form=form)
+
+@health_bp.route('/exercise_record_confirm',methods=['post'])
+def exercise_record_confirm():
+    form=ExerciseRecord()
+    
+    if form.validate_on_submit():
+        session['exercise_date'] = form.date.data
+        session['exercise_time'] = form.exercise_time.data
+        session['exercise_detail'] = form.exercise_detail.data
+        return render_template('health/exercise_record_confirm.html')
+    else:
+        return render_template('health/exercise_record.html',form=form)
+    
+
+@health_bp.route('/exercise_record_execute')
+def exercise_record_execute():
+    form= ExerciseRecord()
+    date = session.get('exercise_date')
+    exercise_time = session.get('exercise_time')
+    exercise_detail = session.get('exercise_detail')
+    user_id = current_user.get_id()
+    
+    count = db.insert_exercise_record(user_id, date, exercise_time, exercise_detail)
+
+    if count == 1:
+        flash("運動内容を記録しました",)
+        return redirect(url_for('user_top'))
+    else:
+        return render_template('health/exercise_record.html',form=form)
+    
